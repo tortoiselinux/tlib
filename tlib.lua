0cf1786d4969d705a5829852de7645ae12fabdbf
wellyton-xs <wellyton.offcer@gmail.com>
2025-01-06 11:31:17 -0300

    tlib update and rockspec removal.
    This update adds functions to handle command line arguments, such as:
    
    	* check_args
    	* verify_args
    	* get_arg
    
    and the most interesting function in my opinion, the function to import
    modules using relative and absolute paths.
    
    I intend to create more functions to manage modules, for now we have:
    
    	* import
    	* check_lib
    
    For the next update I hope to improve the existing functions and bring more
    for handling files and directories.

--- tlib.lua
+++ tlib.lua
@@ -6,6 +6,25 @@
 -- ========================{ END }========================|
 local tlib = {}
 
+function tlib.import(full_path)
+   local path, modname
+   
+   if full_path:match("^%./") then
+      path, modname = full_path:match("^(.*/)([^/]+)$")
+      path = path or "./"
+   else
+      path, modname = full_path:match("^(.-)/([^/]+)$")
+   end
+   
+   if not modname then
+      error("Caminho inválido. Certifique-se de que há um nome de módulo válido.")
+   end
+   
+   package.path = package.path .. string.format(";./%s/?.lua", path)
+
+   return require(modname), package.path, path, modname
+end
+
 function tlib.parse_args(...)
    local args = {...}
    local cmd = table.concat(args, " ")
@@ -20,13 +39,67 @@ end
 
 function tlib.run(...)
    local cmd = tlib.parse_args(...)
+   -- print(cmd)
    local out = io.popen(cmd .. " 2>&1")
    local result = out:read("*a")
    local success, exit_code = out:close()
    if success == nil then
       success = false
    end
-   return result, success, exit_code
+   return result, success, exit_code, cmd
+end
+
+function tlib.write_file(filename, access, content)
+   local file = io.open(filename, access)
+   file:write(content)
+   file:close()
+end
+
+function tlib.read_file(filename)
+   local file = io.open(filename)
+   content = file:read("*a")
+   return content
+end
+
+function tlib.file_exist(filename)
+   local file = io.open(filename)
+   if file then
+      return true
+   else
+      return false
+   end
+end
+
+function tlib.check_args(argi, expected, func)
+   for _, v in ipairs(expected) do
+      if argi == v then
+         func()
+         break
+      end
+   end
+end
+
+function tlib.verify_args(argi, expected)
+   for ai, av in ipairs(argi) do
+      for ei, ev in ipairs(expected) do
+	 if argi[ai] == ev then
+	    return true
+	 end
+      end
+   end
+   return false
+end
+
+function tlib.get_arg(argi, index)
+   for _, v in ipairs(argi) do
+      return argi[index] 
+   end
+end
+
+function tlib.check_lib(lib_table)
+   for k, v in pairs(lib_table) do
+      print(k, v)
+   end
 end
 
 return tlib
